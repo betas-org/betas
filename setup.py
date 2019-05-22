94253848517622a94d5fbeba1edc6ff1fd16e82e
Joel Stremmel <jstremme@uw.edu>
2019-05-21 19:50:31 -0700

    update authors in setup.py to be all of us

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ setup(
   name = 'betas',
   packages = ['betas'],
   version = 'v0.1',      
-  license='MIT',
+  license = 'MIT',
   description = 'This package allows users to visualize model performance, model fit, or model assumptions with one line of code by creating an instance of a plotting class and reusing that instance for various plotting methods.',
-  author = 'Joel Stremmel',
-  author_email = 'jstremme@uw.edu',
+  author = 'Joel Stremmel, Yiming Liu, Cathy Jia, Monique Bi, Arjun Singh',
+  author_email = 'jstremme@uw.edu, liuy379@uw.edu, cathyjia@uw.edu, mybi@uw.edu, arjuns13@uw.edu',
   url = 'https://github.com/betas-org/betas',
   download_url = 'https://github.com/betas-org/betas/archive/v0.1.tar.gz',
-  keywords = ['data science', 'machine learning', 'data visualization', 'visualization', 'model performance', 'model evaluation']
-  install_requires=[
+  keywords = ['data science', 'machine learning', 'data visualization', 'visualization', 'model performance', 'model evaluation'],
+  install_requires = [
           'numpy',
           'pandas',
           'matplotlib',
           'seaborn',
           'scikit-learn'
       ],
-  classifiers=[
+  classifiers = [
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
