a907cb0c03644104abf64cafb8db800bbf600470
Arjun Singh <singh.arjun1313@gmail.com>
2019-05-21 20:30:02 -0700

    Adding Python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 )
