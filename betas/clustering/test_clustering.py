9643620e8171cb02b3b32a98a166932552c6765d
Arjun Singh <singh.arjun1313@gmail.com>
2019-06-03 03:35:35 -0700

    Making the clustering module's code conform to pylint and pycodestyle

--- betas/clustering/test_clustering.py
+++ betas/clustering/test_clustering.py
@@ -1,19 +1,32 @@
-import os
+"""
+Module for testing the clustering_evaluate module, which
+evaluates the different number of clusters for a given
+dataset
+"""
+
 import unittest
 
 from sklearn.datasets.samples_generator import make_blobs
 import clustering_evaluate
 
-class test_kmeans_eval(unittest.TestCase):
+
+class TestKmeansEval(unittest.TestCase):
+    """
+    Testing the function to run k-means++ clustering algo
+    """
     def test_kmeans(self):
         """
-        Testing the function to run k-means++ clustering algo for various number of
-        clusters on the given input_features
+        Testing the function to run k-means++ clustering algo for
+        various number of clusters on the given input_features
         """
         selected_n_clusters = 10
-        X, y = make_blobs(n_samples=10000, centers=selected_n_clusters, n_features=300,
-            cluster_std=0.001, random_state=0)
+        x_train, _y_train = make_blobs(n_samples=10000,
+                                       centers=selected_n_clusters,
+                                       n_features=300,
+                                       cluster_std=0.001,
+                                       random_state=0)
 
-        fig, opt_clusters = clustering_evaluate.kmeans_viz_and_opt_clusters(X)
-        
+        _fig, opt_clusters = \
+            clustering_evaluate.kmeans_viz_and_opt_clusters(x_train)
+
         self.assertAlmostEqual(selected_n_clusters, opt_clusters)
