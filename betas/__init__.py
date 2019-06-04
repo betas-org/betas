031b3791689b9e41bbcb94a76483e8309fba0787
Joel Stremmel <jstremme@uw.edu>
2019-06-04 15:51:19 -0700

    temporary work around to make package work :)

--- betas/__init__.py
+++ betas/__init__.py
@@ -1,4 +1,5 @@
 from .binary_classification import binary_score_plot
 from .clustering import clustering_evaluate
-from .linear_regression import analysis_plot, model_diagnostics
+from .linear_regression import analysis_plot
+# from .linear_regression import model_diagnostics
 from .pca import pca_evaluate
