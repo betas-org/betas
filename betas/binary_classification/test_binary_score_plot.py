1d7ae2d5de359610291793046d904a88f5fb2b3e
Yiming Liu <liuy379@uw.edu>
2019-06-04 11:28:57 -0700

    New test data

--- betas/binary_classification/test_binary_score_plot.py
+++ betas/binary_classification/test_binary_score_plot.py
@@ -1,25 +1,27 @@
 import numpy as np
-
+import pandas as pd
 from binary_score_plot import binary_score_plot
 
 
 def main():
-	'''
-	Runs some sample labels and scores through
-	the plot_pr_by_threshold() function,
-	the plot_hist() function,
-	and the plot_roc() function
-	of the binary_score_plot class.
-	'''
+    '''
+    Runs some sample labels and scores through
+    the plot_pr_by_threshold() function,
+    the plot_hist() function,
+    and the plot_roc() function
+    of the binary_score_plot class.
+    '''
 
-	labels = np.array([1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1])
-	scores = np.array([.78, 0.34, .98, 0.12, .77, .68, .65, .87, .13, .05, .03, .10, .14, .45, .78, .32, .99, .45])
+    df = pd.read_csv('spam_output.csv')
+    scores = df.scores
+    labels = df.actual_label
+    threshold = 0.55
 
-	bsp = binary_score_plot(scores, labels)
-	bsp.plot_hist()
-	bsp.plot_pr_by_threshold()
-	bsp.plot_roc()
+    bsp = binary_score_plot(scores, labels, threshold)
+    bsp.plot_hist()
+    bsp.plot_jitter()
+    bsp.plot_pr_by_threshold()
+    bsp.plot_roc()
 
 if __name__ == '__main__':
-
-	main()
+    main()
