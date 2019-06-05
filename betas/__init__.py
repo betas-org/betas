4b998764b301e1c5e7a414fc6045f01166d45f1b
Joel Stremmel <jstremme@uw.edu>
2019-06-04 17:25:46 -0700

    update to init

--- betas/__init__.py
+++ betas/__init__.py
@@ -1,5 +1,5 @@
-from .binary_classification import binary_score_plot
-from .clustering import clustering_evaluate
-from .linear_regression import analysis_plot
-# from .linear_regression import model_diagnostics
-from .pca import pca_evaluate
+from .binary_classification.binary_score_plot import binary_score_plot
+from .clustering import *
+from .linear_regression.analysis_plot import analysis_plot
+from .linear_regression.model_diagnostics import model_diagnostics
+from .pca import *
