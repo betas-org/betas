e7f428896aa3f691f064f8397c2ba07b210b3358
Mengying77 <mybi@uw.edu>
2019-06-09 14:28:25 -0700

    fix import

--- betas/pca_evaluate.py
+++ betas/pca_evaluate.py
@@ -5,14 +5,12 @@ differ as we vary the number of dimensions of the input data
 
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('seaborn-white')
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegressionCV
-
+plt.style.use('seaborn')
 
 def sigmoid(features):
     """
