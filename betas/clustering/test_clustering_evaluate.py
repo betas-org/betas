7e180c0b59745302185d3e69c3341f9e762355ef
Cathy Jia <qiaoyu_jia@yahoo.com>
2019-06-08 17:40:09 -0700

    change testcase name

--- betas/clustering/test_clustering_evaluate.py
+++ betas/clustering/test_clustering_evaluate.py
@@ -10,6 +10,7 @@ class TestKmeansEval(unittest.TestCase):
     """
     Testing the function to run k-means++ clustering algo
     """
+    
     def test_kmeans(self):
         """
         Testing the function to run k-means++ clustering algo for
@@ -21,8 +22,25 @@ class TestKmeansEval(unittest.TestCase):
                                        n_features=300,
                                        cluster_std=0.001,
                                        random_state=0)
-
-        plt_clust, opt_clusters = \
+            
+        opt_clusters = \
             clustering_evaluate.kmeans_viz_and_opt_clusters(x_train,
-                                                            plot_figure=True)
+                                                            plot_figure=False)
         self.assertTrue(selected_n_clusters == opt_clusters)
+
+    def test_kmeans_figure(self):
+        """
+            Testing the function to run k-means++ clustering algo for
+            various number of clusters on the given input_features
+            """
+        selected_n_clusters = 10
+        x_train, _y_train = make_blobs(n_samples=10000,
+                                       centers=selected_n_clusters,
+                                       n_features=300,
+                                       cluster_std=0.001,
+                                       random_state=0)
+            
+       plt_clust, opt_clusters = \
+           clustering_evaluate.kmeans_viz_and_opt_clusters(x_train,
+                                                           plot_figure=True)
+       self.assertTrue(selected_n_clusters == opt_clusters)
